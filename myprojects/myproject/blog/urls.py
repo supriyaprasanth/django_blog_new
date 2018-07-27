14f652a2e45aaf187910acf7b1d1dea47871754c
supriyaprasanth <supriya.p@inapp.com>
2018-07-27 15:17:45 +0530

    blog w/o chat and notification

--- myprojects/myproject/blog/urls.py
+++ myprojects/myproject/blog/urls.py
@@ -5,4 +5,11 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+
 ]
