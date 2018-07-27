a89593bb4a1e3e9ccb7cc3ef21e57fb76d2274d0
supriyaprasanth <supriya.p@inapp.com>
2018-07-27 17:20:43 +0530

    blog wo chat and noti

--- myprojects/myproject/blog/models.py
+++ myprojects/myproject/blog/models.py
@@ -18,8 +18,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def approved_comments(self):
-    	return self.comments.filter(approved_comment=True)
+   
 
 
 class Comment(models.Model):
@@ -27,12 +26,17 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=False)
+
+    #approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_comment = True
+        #elf.approved_comment = True
         self.save()
+
 
     def __str__(self):
         return self.text
-
+'''
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+'''
