1f4aaa153f762d32a83f3804610dd93292539a38
supriyaprasanth <supriya.p@inapp.com>
2018-07-30 16:20:01 +0530

    new blog

--- myprojects/myproject/blog/models.py
+++ myprojects/myproject/blog/models.py
@@ -8,12 +8,6 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
@@ -23,20 +17,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, default='xxxxx')
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
 
-    #approved_comment = models.BooleanField(default=False)
-
-    def approve(self):
-        #elf.approved_comment = True
-        self.save()
-
-
     def __str__(self):
         return self.text
-'''
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
-'''
--- myprojects/myproject/blog/views.py
+++ myprojects/myproject/blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 @login_required
@@ -22,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.created_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,17 +37,20 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+    
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_remove(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	post.delete()
-	return redirect('post_list')
+@login_required
+def post_remove(request, pk):  
+    post = get_object_or_404(Post, pk=pk)
+    #item = Post.objects.get(pk=post.id)
+    if request.user == post.author:
+        post.delete()
+    return redirect('post_list')        
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -56,8 +59,10 @@ def add_comment_to_post(request, pk):
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
+            comment.author = request.user
             comment.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
+
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
